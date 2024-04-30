d491f7d19f210c08d42fd2d1ddb0847e0fa62f0b
vtAndreola <dev.andreola@gmail.com>
2024-04-29 22:09:21 -0300

    style: category list overflow

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -4,10 +4,13 @@ import CategoryItem from "./category-item";
 const CategoryList = async () => {
   const categories = await db.category.findMany({});
   return (
-    <div className="grid grid-cols-2 gap-3">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
+    <div className="flex overflow-x-scroll pb-5 scrollbar-hide">
+      <div className="mx-5 flex gap-3 lg:mx-auto">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+        <div className="min-w-2"></div>
+      </div>
     </div>
   );
 };
