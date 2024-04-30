1ad9cc9e2d48d551c9d53ead4f49b57f1610ca81
vtAndreola <dev.andreola@gmail.com>
2024-04-29 23:57:51 -0300

    feat: add second banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,6 +5,7 @@ import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import Banner from "./_components/banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -27,23 +27,19 @@ const Home = async () => {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
+
       <div className="pt-6">
         <CategoryList />
       </div>
+
       <div className="px-5">
-        <Image
-          src={"/promo-banner.png"}
-          height={0}
-          width={0}
-          alt="Até 30% de desconto em pizzas!"
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
-        />
+        <Banner src="/promo-banner.png" alt="Até 30% de desconto em pizzas!" />
       </div>
+
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
@@ -56,6 +52,13 @@ const Home = async () => {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Banner
+          src="/burguer-banner.png"
+          alt="A partir de R$17,90 em lanches"
+        />
       </div>
     </>
   );
