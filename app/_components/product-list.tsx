75f734dea18652f343ed1c98afddcdb7a966cf92
vtAndreola <dev.andreola@gmail.com>
2024-04-29 23:45:02 -0300

    feat: create featured products carousel

---
+++ app/_components/product-list.tsx
@@ -0,0 +1,26 @@
+import { Prisma } from "@prisma/client";
+import ProductItem from "./product-item";
+
+interface ProductListProps {
+  products: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
+
+const ProductList = async ({ products }: ProductListProps) => {
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 scrollbar-hide">
+      {products.map((product) => (
+        <ProductItem product={product} key={product.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -23,6 +43,19 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList products={products} />
       </div>
     </>
   );
