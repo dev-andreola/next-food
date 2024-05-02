44f3a0968b894546a899e41d8741efc777292fe5
vtAndreola <dev.andreola@gmail.com>
2024-05-02 09:34:43 -0300

    fix: deal with no result on searching

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { Restaurant } from "@prisma/client";
-import { notFound, useSearchParams } from "next/navigation";
+import { notFound, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "./_actions/search";
 import Header from "../_components/header";
 import RestaurantItem from "../_components/restaurant-item";
+import { Button } from "../_components/ui/button";
 
 const Restaurants = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
 
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
@@ -35,14 +37,21 @@ const Restaurants = () => {
       <div className="px-5 py-6">
         <h2 className="mb-6 text-xl font-semibold">Restaurantes Encontrados</h2>
         <div className="flex w-full flex-col gap-6 ">
-          {restaurants.map((restaurant) => (
-            <RestaurantItem
-              key={restaurant.id}
-              restaurant={restaurant}
-              className="min-w-full max-w-full"
-            />
-          ))}
+          {restaurants.length > 0 ? (
+            restaurants.map((restaurant) => (
+              <RestaurantItem
+                key={restaurant.id}
+                restaurant={restaurant}
+                className="min-w-full max-w-full"
+              />
+            ))
+          ) : (
+            <p className="text-muted-foreground">Restaurante não encontrado</p>
+          )}
         </div>
+        <Button className="mt-5 w-full" onClick={router.back}>
+          Voltar
+        </Button>
       </div>
     </>
   );
