4745c63ca8a094df80432624cc334d6bcc352459
vtAndreola <dev.andreola@gmail.com>
2024-05-13 18:32:43 -0300

    feat: ensure restaurants can be favorited

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,12 +1,27 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
 import RestaurantItem from "./restaurant-item";
+import { authOptions } from "../_lib/auth";
 
 const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
+
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
+
+  console.log(userFavoriteRestaurants);
+
   return (
-    <div className="flex gap-4 overflow-x-scroll px-5 scrollbar-hide">
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
-        <RestaurantItem restaurant={restaurant} key={restaurant.id} />
+        <RestaurantItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          userId={session?.user?.id}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
       ))}
     </div>
   );
