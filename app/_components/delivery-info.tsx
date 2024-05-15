b9a6c92d96b7a29e4b9de93da19c5c7421e38ceb
vtAndreola <dev.andreola@gmail.com>
2024-05-15 15:21:07 -0300

    feat: restaurant page favorite button

--- app/_components/delivery-info.tsx
+++ app/_components/delivery-info.tsx
@@ -1,46 +1,45 @@
 import { BikeIcon, TimerIcon } from "lucide-react";
 import { Card } from "./ui/card";
 import { formatCurrency } from "../_helpers/price";
-import { Decimal } from "@prisma/client/runtime/library";
+import { Restaurant } from "@prisma/client";
 
 interface DeliveryInfoProps {
-  deliveryFee: Decimal;
-  deliveryTimeMinutes: number;
+  restaurant: Pick<Restaurant, "deliveryFee" | "deliveryTimeMinutes">;
 }
 
-const DeliveryInfo = ({
-  deliveryFee,
-  deliveryTimeMinutes,
-}: DeliveryInfoProps) => {
+const DeliveryInfo = ({ restaurant }: DeliveryInfoProps) => {
   return (
-    <Card className="mt-6 flex justify-around py-2">
-      {/* FEE */}
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex items-center gap-1 text-muted-foreground">
-          <span className="text-xs">Entrega</span>
-          <BikeIcon size={14} />
+    <>
+      <Card className="mt-6 flex justify-around py-3">
+        {/* CUSTO */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <BikeIcon size={14} />
+          </div>
+
+          {Number(restaurant.deliveryFee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formatCurrency(Number(restaurant.deliveryFee))}
+            </p>
+          ) : (
+            <p className="text-xs font-semibold">Grátis</p>
+          )}
         </div>
 
-        {Number(deliveryFee) > 0 ? (
-          <p className="text-xs font-semibold">
-            {formatCurrency(Number(deliveryFee))}
-          </p>
-        ) : (
-          <p className="text-xs font-semibold">Grátis</p>
-        )}
-      </div>
-      {/* TIME */}
-      <div>
-        <div className="flex flex-col items-center justify-center">
+        {/* TEMPO */}
+        <div className="flex flex-col items-center">
           <div className="flex items-center gap-1 text-muted-foreground">
             <span className="text-xs">Entrega</span>
             <TimerIcon size={14} />
           </div>
 
-          <p className="text-xs font-semibold">{deliveryTimeMinutes}min</p>
+          <p className="text-xs font-semibold">
+            {restaurant.deliveryTimeMinutes} min
+          </p>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 };
 
--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -6,6 +6,8 @@ import { StarIcon } from "lucide-react";
 import DeliveryInfo from "@/app/_components/delivery-info";
 import ProductList from "@/app/_components/product-list";
 import CartBanner from "./_components/cart-banner";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 interface RestaurantPageProps {
   params: {
@@ -54,42 +56,50 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
   if (!restaurant) {
     return notFound();
   }
+  const session = await getServerSession(authOptions);
+
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+  });
 
   return (
-    <div className="mb-5">
-      <RestaurantImage name={restaurant.name} imageUrl={restaurant.imageUrl} />
+    <div>
+      <RestaurantImage
+        restaurant={restaurant}
+        userFavoriteRestaurants={userFavoriteRestaurants}
+      />
 
-      <div className="relative z-50 mt-[-1.5rem] flex items-center justify-between rounded-t-3xl bg-white px-5 pt-5">
+      <div className="relative z-50 mt-[-1.5rem] flex items-center justify-between rounded-tl-3xl rounded-tr-3xl bg-white px-5 pt-5">
+        {/* TITULO */}
         <div className="flex items-center gap-[0.375rem]">
           <div className="relative h-8 w-8">
             <Image
-              className="rounded-full object-cover"
               src={restaurant.imageUrl}
-              fill
               alt={restaurant.name}
+              fill
+              className="rounded-full object-cover"
             />
           </div>
           <h1 className="text-xl font-semibold">{restaurant.name}</h1>
         </div>
 
         <div className="flex items-center gap-[3px] rounded-full bg-foreground px-2 py-[2px] text-white">
-          <StarIcon className="fill-yellow-400 text-yellow-400" size={12} />
+          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
           <span className="text-xs font-semibold">5.0</span>
         </div>
       </div>
 
       <div className="px-5">
-        <DeliveryInfo
-          deliveryFee={restaurant.deliveryFee}
-          deliveryTimeMinutes={restaurant.deliveryTimeMinutes}
-        />
+        <DeliveryInfo restaurant={restaurant} />
       </div>
 
-      <div className="mt-3 flex gap-4 overflow-x-scroll px-5 scrollbar-hide">
+      <div className="mt-3 flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
         {restaurant.categories.map((category) => (
           <div
-            className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
             key={category.id}
+            className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
           >
             <span className="text-xs text-muted-foreground">
               {category.name}
@@ -99,16 +109,19 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
       </div>
 
       <div className="mt-6 space-y-4">
-        <h2 className="ml-5 font-semibold">Mais Pedidos</h2>
+        {/* TODO: mostrar produtos mais pedidos quando implementarmos realização de pedido */}
+        <h2 className="px-5  font-semibold">Mais Pedidos</h2>
         <ProductList products={restaurant.products} />
       </div>
 
       {restaurant.categories.map((category) => (
         <div className="mt-6 space-y-4" key={category.id}>
-          <h2 className="ml-5 font-semibold">{category.name}</h2>
+          {/* TODO: mostrar produtos mais pedidos quando implementarmos realização de pedido */}
+          <h2 className="px-5  font-semibold">{category.name}</h2>
           <ProductList products={category.products} />
         </div>
       ))}
+
       <CartBanner restaurant={restaurant} />
     </div>
   );
