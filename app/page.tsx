2adc3e98e56b6966258507718e2cfadca604d568
vtAndreola <dev.andreola@gmail.com>
2024-05-15 15:50:59 -0300

    perf: add promise all to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
-import Banner from "./_components/banner";
+import PromoBanner from "./_components/promo-banner";
 import RestaurantList from "./_components/restaurant-list";
 import Link from "next/link";
 
-const Home = async () => {
-  const products = await db.product.findMany({
+const fetch = async () => {
+  const getProducts = db.product.findMany({
     where: {
       discountPercentage: {
         gt: 0,
@@ -26,20 +26,48 @@ const Home = async () => {
     },
   });
 
+  const getBurguersCategory = db.category.findFirst({
+    where: {
+      name: "Hambúrgueres",
+    },
+  });
+
+  const getPizzasCategory = db.category.findFirst({
+    where: {
+      name: "Pizzas",
+    },
+  });
+
+  const [products, burguersCategory, pizzasCategory] = await Promise.all([
+    getProducts,
+    getBurguersCategory,
+    getPizzasCategory,
+  ]);
+
+  return { products, burguersCategory, pizzasCategory };
+};
+
+const Home = async () => {
+  const { products, burguersCategory, pizzasCategory } = await fetch();
+
   return (
     <>
       <Header />
-
       <div className="px-5 pt-6">
         <Search />
       </div>
 
-      <div className="pt-6">
+      <div className="px-5 pt-6">
         <CategoryList />
       </div>
 
-      <div className="px-5">
-        <Banner src="/promo-banner.png" alt="Até 30% de desconto em pizzas!" />
+      <div className="px-5 pt-6">
+        <Link href={`/categories/${pizzasCategory?.id}/products`}>
+          <PromoBanner
+            src="/promo-banner-01.png"
+            alt="Até 30% de desconto em pizzas!"
+          />
+        </Link>
       </div>
 
       <div className="space-y-4 pt-6">
@@ -57,15 +85,16 @@ const Home = async () => {
             </Link>
           </Button>
         </div>
-
         <ProductList products={products} />
       </div>
 
       <div className="px-5 pt-6">
-        <Banner
-          src="/burguer-banner.png"
-          alt="A partir de R$17,90 em lanches"
-        />
+        <Link href={`/categories/${burguersCategory?.id}/products`}>
+          <PromoBanner
+            src="/promo-banner-02.png"
+            alt="A partir de R$17,90 em lanches"
+          />
+        </Link>
       </div>
 
       <div className="space-y-4 py-6">
@@ -83,7 +112,6 @@ const Home = async () => {
             </Link>
           </Button>
         </div>
-
         <RestaurantList />
       </div>
     </>
