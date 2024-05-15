505f1fd071750bc16f10e44e5fb42f12487584e6
vtAndreola <dev.andreola@gmail.com>
2024-05-15 16:08:22 -0300

    fix: restaurant list

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -6,12 +6,11 @@ import { authOptions } from "../_lib/auth";
 const RestaurantList = async () => {
   const session = await getServerSession(authOptions);
 
+  // TODO: pegar restaurantes com maior número de pedidos
   const restaurants = await db.restaurant.findMany({ take: 10 });
   const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
     where: { userId: session?.user?.id },
   });
-
-  console.log(userFavoriteRestaurants);
 
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
@@ -19,7 +18,6 @@ const RestaurantList = async () => {
         <RestaurantItem
           key={restaurant.id}
           restaurant={restaurant}
-          userId={session?.user?.id}
           userFavoriteRestaurants={userFavoriteRestaurants}
         />
       ))}
