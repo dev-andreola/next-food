393ef2228a6ee8b2b1c3f612552c74536988344f
vtAndreola <dev.andreola@gmail.com>
2024-06-18 15:52:33 -0300

    style: add recommended products page a desktop layout

--- app/products/recommended/page.tsx
+++ app/products/recommended/page.tsx
@@ -19,23 +19,21 @@ const RecommendedProducts = async () => {
     },
   });
   return (
-    <div>
-      <>
-        <Header />
-        <div className="px-5 py-6">
-          <h2 className="mb-6 text-xl font-semibold">Pedidos Recomendados</h2>
-          <div className="grid w-full grid-cols-2 gap-6 ">
-            {products.map((product) => (
-              <ProductItem
-                key={product.id}
-                product={product}
-                className="min-w-full"
-              />
-            ))}
-          </div>
+    <>
+      <Header />
+      <div className="mx-auto max-w-6xl px-5 py-6">
+        <h2 className="mb-6 text-xl font-semibold">Pedidos Recomendados</h2>
+        <div className="grid w-full grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-6 ">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
         </div>
-      </>
-    </div>
+      </div>
+    </>
   );
 };
 
