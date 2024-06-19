d1ee0f2fd7548c9255639bfae429bf511188eb8a
vtAndreola <dev.andreola@gmail.com>
2024-06-19 16:27:30 -0300

    feat: fininsh navbar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -23,8 +23,15 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Separator } from "./ui/separator";
 import Search from "./search";
 
+import { PiHamburger, PiPizza, PiForkKnife } from "react-icons/pi";
+import { BiSushi } from "react-icons/bi";
+import { BsCupStraw } from "react-icons/bs";
+import { TbCandy } from "react-icons/tb";
+import { usePathname } from "next/navigation";
+
 const Header = () => {
   const { data } = useSession();
+  const pathname = usePathname();
 
   const handleSignOutClick = () => signOut();
   const handleSignInClick = () => signIn();
@@ -97,23 +104,26 @@ const Header = () => {
             </>
           )}
 
-          <div className="py-6">
+          <div className="py-3">
             <Separator />
           </div>
 
           <div className="space-y-2">
             <Button
-              variant="ghost"
+              variant={pathname === "/" ? "default" : "ghost"}
               className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
             >
-              <HomeIcon size={16} />
-              <span className="block">Início</span>
+              <Link href="/">
+                <HomeIcon size={16} />
+                <span className="block">Início</span>
+              </Link>
             </Button>
 
             {data?.user && (
               <>
                 <Button
-                  variant="ghost"
+                  variant={pathname === "/my-orders" ? "default" : "ghost"}
                   className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
                   asChild
                 >
@@ -124,7 +134,11 @@ const Header = () => {
                 </Button>
 
                 <Button
-                  variant="ghost"
+                  variant={
+                    pathname === "/my-favorite-restaurants"
+                      ? "default"
+                      : "ghost"
+                  }
                   className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
                   asChild
                 >
@@ -137,7 +151,114 @@ const Header = () => {
             )}
           </div>
 
-          <div className="py-6">
+          <div className="py-3">
+            <Separator />
+          </div>
+
+          <div className="space-y-2">
+            <Button
+              variant={
+                pathname ===
+                "/categories/f448d4ab-21ee-492e-944a-b1a5b4170ef7/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/f448d4ab-21ee-492e-944a-b1a5b4170ef7/products">
+                <PiHamburger size={16} />
+                <span className="block">Hambúrgueres</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={
+                pathname ===
+                "/categories/cf9ae18b-8dda-41ad-ad40-a1c1c8f8f6c5/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/cf9ae18b-8dda-41ad-ad40-a1c1c8f8f6c5/products">
+                <PiPizza size={16} />
+
+                <span className="block">Pizzas</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={
+                pathname ===
+                "/categories/4cbbb29c-e97a-4734-ac6d-b2dbf1cf67e2/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/4cbbb29c-e97a-4734-ac6d-b2dbf1cf67e2/products">
+                <BiSushi size={16} />
+
+                <span className="block">Japonesa</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={
+                pathname ===
+                "/categories/11e44389-b9f0-4df4-a500-ec3fa939f166/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/11e44389-b9f0-4df4-a500-ec3fa939f166/products">
+                <PiForkKnife size={16} />
+
+                <span className="block">Brasileira</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={
+                pathname ===
+                "/categories/69b9043c-3915-49fa-9e7d-8ee4aeb088f7/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/69b9043c-3915-49fa-9e7d-8ee4aeb088f7/products">
+                <BsCupStraw size={16} />
+
+                <span className="block">Sucos</span>
+              </Link>
+            </Button>
+
+            <Button
+              variant={
+                pathname ===
+                "/categories/b8cef5ab-f313-4c3f-a2d0-77da94394087/products"
+                  ? "default"
+                  : "ghost"
+              }
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/categories/b8cef5ab-f313-4c3f-a2d0-77da94394087/products">
+                <TbCandy size={16} />
+
+                <span className="block">Sobremesas</span>
+              </Link>
+            </Button>
+          </div>
+
+          <div className="py-3">
             <Separator />
           </div>
 
