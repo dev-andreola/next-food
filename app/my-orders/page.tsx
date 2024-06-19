19c139d859715bbbe2e052e2f25d80c694706e4b
vtAndreola <dev.andreola@gmail.com>
2024-06-19 10:25:57 -0300

    style: my-orders page desktop layout

--- app/my-orders/page.tsx
+++ app/my-orders/page.tsx
@@ -28,12 +28,14 @@ const MyOrdersPage = async () => {
 
   return (
     <>
-      <Header />
+      <div className="lg:border-b-[1px] lg:border-neutral-200 lg:pb-6">
+        <Header />
+      </div>
 
-      <div className="px-5 py-6">
+      <div className="mx-auto max-w-6xl px-5 py-6">
         <h2 className="pb-6 text-lg font-semibold">Meus Pedidos</h2>
 
-        <div className="space-y-4">
+        <div className="gap-2 space-y-4 lg:grid lg:grid-cols-2 lg:justify-stretch lg:space-y-0">
           {orders.map((order) => (
             <OrderItem key={order.id} order={order} />
           ))}
